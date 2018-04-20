8d5421061e7aaf0f74281a779ab231aafdbfb707
promark33 <promark33@gmail.com>
2018-04-21 02:16:52 +0300

    Нормальные таски

--- app/src/main/java/com/lawdev/designal/helpers/TasksData.java
+++ app/src/main/java/com/lawdev/designal/helpers/TasksData.java
@@ -10,12 +10,12 @@ public class TasksData {
     public static ArrayList<Task> finished_tasks = new ArrayList<>();
 
     public TasksData(){
-        tasks.add(new Task("1", "2"));
-        tasks.add(new Task("2", "3"));
-        tasks.add(new Task("4", "5"));
+        tasks.add(new Task("Купить хлеб", "2 батона белого и половину черного"));
+        tasks.add(new Task("Купить MacBook 2017", "И переходники!"));
+        tasks.add(new Task("Купить права", "На категорию B"));
 
-        finished_tasks.add(new Task("1", "2"));
-        finished_tasks.add(new Task("2", "3"));
-        finished_tasks.add(new Task("4", "5"));
+        finished_tasks.add(new Task("Победить в хакатоне", "Занять первое место"));
+        finished_tasks.add(new Task("Узнать, что такое ListView", "В гугле"));
+        finished_tasks.add(new Task("Выучить Java", "Возможное невозможно"));
     }
 }
