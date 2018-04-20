6a48239a17294e5ee80e5f1a567c2bd4e25e805f
Nikita <nikita-reutov@mail.ru>
2018-04-20 12:57:50 +0300

    Пустышка

---
+++ app/src/main/java/com/lawdev/designal/AddActivity.java
@@ -0,0 +1,26 @@
+package com.lawdev.designal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {   //вызывается при выборе элемента из Action bar
+        switch (item.getItemId()) { //выбор действия в соответствии с конкретным элементом меню
+            case android.R.id.home: //выход из приложения
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
