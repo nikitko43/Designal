1937daf27eb2ca1d04d1ca4f876b616883afa70e
promark33 <promark33@gmail.com>
2018-04-21 03:12:37 +0300

    Рабочая версия

--- app/src/main/java/com/lawdev/designal/helpers/GroupsData.java
+++ app/src/main/java/com/lawdev/designal/helpers/GroupsData.java
@@ -9,9 +9,9 @@ import java.util.List;
 public class GroupsData {
     public static ArrayList<Group> groups = new ArrayList<>();
     public GroupsData(){
-        groups.add(new Group("1", "2"));
-        groups.add(new Group("3", "4"));
-        groups.add(new Group("5", "6"));
+        groups.add(new Group("Студенты МГТУ", "2"));
+        groups.add(new Group("Участники BestHack", "4"));
+        groups.add(new Group("Команда LawTeam", "6"));
     }
     public ArrayList<Group> getGroups() {
         return groups;
